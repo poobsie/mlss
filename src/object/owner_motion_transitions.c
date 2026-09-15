c7fb06727f059dfb79c5c0bfd57797a0ab8f32eb
Pooble <90473506+poobsie@users.noreply.github.com>
2026-09-15 09:54:19 -0400

    Decompile sub_80DBB1C owner motion transition

---
+++ src/object/owner_motion_transitions.c
@@ -0,0 +1,67 @@
+#include "object/functions.h"
+#include "object/runtime_object.h"
+
+#define SEC(symbol) \
+    __attribute__((section(".text.owner_motion_transitions." #symbol)))
+
+void sub_807F4FC(struct RuntimeObject* object);
+void sub_8082E1C(
+    struct RuntimeObject* object, s32 animation, s32 command, s32 argument);
+void sub_8085B38(struct RuntimeObject* object);
+void sub_80DBC3C(struct RuntimeObject* object);
+void sub_80DBBCC(struct RuntimeObject* object);
+
+SEC(sub_80DBB1C)
+void sub_80DBB1C(struct RuntimeObject* object)
+{
+    struct ObjectPositionOwner* owner;
+    struct ObjectPositionSource* state;
+    s32* stateX;
+    s32* stateY;
+    s32* stateZ;
+    s32 adjustedPosition;
+    s32 gateValue;
+    s32 position;
+    u16 remaining;
+    u8 flags;
+
+    gateValue = object->value80;
+    if (gateValue != 0)
+        return;
+
+    remaining = object->timer - 1;
+    object->timer = object->timer - 1;
+    if ((s32)((u32)remaining << 16) < 0) {
+        sub_807F4FC(object);
+        object->secondaryUpdate = object_save_update_and_run_setup;
+
+        owner = (struct ObjectPositionOwner*)object->positionOwner;
+        state = owner->positionSource;
+        stateX = &state->positionX;
+        adjustedPosition = (s32)((u32)*stateX
+            + ((u32)object->valueA0 << 8));
+        position = adjustedPosition;
+        object->positionX = position;
+        object->currentPositionX = position;
+        stateY = &state->positionY;
+        position = *stateY;
+        object->positionY = position;
+        object->currentPositionY = position;
+        stateZ = &state->positionZ;
+        position = *stateZ;
+        object->positionZBase = position;
+        object->verticalPosition = position;
+
+        object->value84 = *stateX + 0x400;
+        object->value88 = *stateY;
+        object->value8C = *stateZ;
+        flags = object->flags79;
+        flags |= 0x20;
+        object->flags79 = flags;
+        object->unknown7C = (u16)((u32)object->valueA4 << 8);
+        object->unknown7A = gateValue;
+        sub_8085B38(object);
+        sub_8082E1C(object, 4, 0, 0);
+        object->update = sub_80DBBCC;
+    }
+}
