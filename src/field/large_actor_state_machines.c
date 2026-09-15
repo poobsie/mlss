138f92cd24d34f9263169d0c593f7737d4620f5f
Pooble <90473506+poobsie@users.noreply.github.com>
2026-09-15 09:46:40 -0400

    Decompile sub_80C1AE8

--- src/field/large_actor_state_machines.c
+++ src/field/large_actor_state_machines.c
@@ -43,6 +43,41 @@ void sub_80D782C(void);
 void sub_80C8418(void);
 void sub_80A2918(void);
 void sub_80BC408(struct RuntimeObject* object);
+void sub_80C1BB8(struct RuntimeObject* object);
+void sub_80C1CE4(struct RuntimeObject* object);
+
+#define field_prepare_actor_a_offset_motion_or_idle sub_80C1AE8
+SEC(sub_80C1AE8)
+void field_prepare_actor_a_offset_motion_or_idle(
+    struct RuntimeObject* caller)
+{
+    struct FieldActor* actorA = gFieldRuntime->actorA;
+    struct RuntimeObject* action = (struct RuntimeObject*)&actorA->action;
+    s32 idleState;
+    s32 motionState;
+
+    /* value9A remains a neutral actor field; current evidence only proves that
+     * it tracks the action's display-relative height for this transition. */
+    if (action->visual->flags & OBJECT_VISUAL_COMPLETE) {
+        if (action->positionZBase == 0) {
+            idleState = actorA->stateFlags & 6;
+            if (idleState == 2 || idleState == 4)
+                action->update = sub_80C1CE4;
+        } else {
+            motionState = actorA->stateFlags & 6;
+            if (motionState == 2 || motionState == 4) {
+                sub_808843C(action,
+                    action->positionX / 256 + actorA->displayOffsetX - 0x3A,
+                    action->positionY / 256 + actorA->displayOffsetY,
+                    0, -1);
+                actorA->value9A = action->positionZBase / 256 + 8;
+                sub_8088164(action, 0x500);
+            }
+            action->secondaryTimer &= 0xFFF;
+            caller->update = sub_80C1BB8;
+        }
+    }
+}
 
 SEC(sub_80BC37C)
 void sub_80BC37C(struct RuntimeObject* caller)
