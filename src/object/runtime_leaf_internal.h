80e059e81ab3b19b22c54bd42a459720d32b1845
Pooble <90473506+poobsie@users.noreply.github.com>
2026-09-20 23:36:35 -0400

    Split linked effect runtime callbacks

---
+++ src/object/runtime_leaf_internal.h
@@ -0,0 +1,24 @@
+#ifndef GUARD_OBJECT_RUNTIME_LEAF_INTERNAL_H
+#define GUARD_OBJECT_RUNTIME_LEAF_INTERNAL_H
+
+#include "object/runtime_object.h"
+
+void sub_807C298(struct RuntimeObject* object);
+void sub_8082E1C(
+    struct RuntimeObject* object, s32 animation, s32 command, s32 argument);
+void sub_80DF024(
+    s32 effect, s32 x, s32 y, s32 z, struct RuntimeObject* object);
+void sub_810DD7C(
+    struct RuntimeObject* object, void* owner, s32 command);
+u32 sub_8199F30(void);
+
+void sub_80761E8(struct RuntimeObject* object);
+void sub_8076338(struct RuntimeObject* object);
+void sub_8076414(struct RuntimeObject* object);
+void sub_80764D8(struct RuntimeObject* object);
+void sub_8076C14(struct RuntimeObject* object);
+void sub_8076D90(struct RuntimeObject* object);
+void sub_807777C(struct RuntimeObject* object);
+void sub_8077924(struct RuntimeObject* object);
+
+#endif
